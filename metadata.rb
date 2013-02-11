4269e8b2e57b4a8f6920fc014ad6e4e24e6b9cd4
frekofefe <frekofefe@gmail.com>
2013-02-11 15:47:21 +0100

    Implementing: Packages install & dowload GeoLiteCity.dat.gz

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,11 @@ license          'All rights reserved'
 description      'Installs/Configures ossim-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w{ debian ubuntu centos suse fedora redhat }.each do |os|
+    supports os
+end
+
+%w{ apt yum }.each do |dep|
+    depends dep
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,32 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+geolitecity[:url] = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
+geolitecity[:path] = "/usr/share/geoip"
+geolitecity[:file] = "GeoLiteCity.dat"
+
+# packages
+%{ git wget python-geoip python-pyinotify python-tz python-nmap python-ldap python-libpcap }.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+# OssinAgent need "/usr/share/geoip/GeoLiteCity.dat"
+directory "/usr/share/geoip" do
+    owner "root"
+    group "root"
+    mode 00755
+    action :create
+end
+
+bash "download GeoLiteCity.dat" do
+    user "root"
+    cwd geolitecity[:path]
+    code <<-EOH
+        wget -c -t3 #{geolitecity[:url]}
+        gunzip #{geolitecity[:file]}.gz
+    EOH
+end
+
