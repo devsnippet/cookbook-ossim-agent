6f51a784e23e6798593be639bdfcda3b04e7d76f
frekofefe <frekofefe@gmail.com>
2013-02-18 17:06:41 +0100

    Ossim Plugins management

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,3 +18,14 @@ default[:ossim][:release] = "release-4.1.3"
 default[:ossim][:server] = "127.0.0.1"
 default[:ossim][:port] = "40001"
 
+#
+# Framework
+#
+default[:ossim][:control_framework] = "False"
+default[:ossim][:frameworkd][:server_ip] = "127.0.0.1"
+default[:ossim][:frameworkd][:server_port] = "40003"
+
+#
+# Plugins
+#
+default[:ossim][:plugins] =  {:p0f => { :enable => true, :path => "/etc/ossim/agent/plugins/p0f.cfg" }}
