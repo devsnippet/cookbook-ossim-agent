f365d7454a412f4f0f950c95b3b096343686141d
frekofefe <frekofefe@gmail.com>
2013-02-19 16:22:37 +0100

    Change the platforms supported

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,13 @@
 name             'ossim-agent'
-maintainer       'Abadasoft'
+maintainer       'Fernando Israel García Martínez'
 maintainer_email 'figarciamartinez@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures ossim-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.10.0'
 
-%w{ debian ubuntu centos suse fedora redhat }.each do |os|
+# %w{ debian ubuntu centos suse fedora redhat }.each do |os|
+# centos suse fedora redhat not tested
+%w{ debian ubuntu }.each do |os|
     supports os
 end
