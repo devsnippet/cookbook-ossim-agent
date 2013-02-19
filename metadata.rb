9d1ea15e456eb36bf2b8bf37b948657e93db8806
frekofefe <frekofefe@gmail.com>
2013-02-19 13:26:21 +0100

    Change metada version to 0.10.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'figarciamartinez@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures ossim-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.10.0'
 
 %w{ debian ubuntu centos suse fedora redhat }.each do |os|
     supports os
